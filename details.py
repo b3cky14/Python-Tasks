47b9e8113de3c9943177d4acf87908fd5b5413b1
b3cky14 <153016182+b3cky14@users.noreply.github.com>
2023-12-05 20:32:03 +0000

    Add files via upload

---
+++ details.py
@@ -0,0 +1,20 @@
+#create name, age, house number and street name variables
+#of the user using input function
+#print a single sentence by adding the variables together.
+
+#name variable of users name
+user_name = input("What is your first and last name? ")
+
+#age variable of users age
+age = input("What is your age? ")
+
+#house number variable of users house number
+house_number = input("What is your house number? ")
+
+#street name varaible of users street name
+street_name = input("What is the name of your street? ")
+
+#print all the variables to convey the users details
+print("This is " + user_name + ". They are " + age + 
+      " years old and live at house number " + house_number +
+      " on " + street_name + ".")
