14e06da6f58f823bc9968ad6f4481f7afc7055ff
b3cky14 <153016182+b3cky14@users.noreply.github.com>
2023-12-07 18:10:47 +0000

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,11 @@
+#create variable for the string used in the pattern
+
+string = "*"
+  
+#create for loop with a range
+
+for i in range(1,10):
+    if i <=5:
+        print(i*string)         #1,2,3,4,5 * string
+    else:
+        print((10-i)*string)      #4,3,2,1 * string
