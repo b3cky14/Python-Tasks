451372b4c1fee67f4122275b472dbb06673a2dc5
b3cky14 <153016182+b3cky14@users.noreply.github.com>
2023-12-27 11:22:28 +0000

    Add files via upload

---
+++ First-Computer-Program-and-Variables/conversion.py
@@ -0,0 +1,15 @@
+# create the 4 variables stated
+# convert the variables using str() int() float() functions
+# print out the variables one at a time
+
+# create the variables
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# convert the variables and print them
+print(int(num1))
+print(float(num2))
+print(str(num3))
+print(int(string1))
---
+++ First-Computer-Program-and-Variables/hello_world.py
@@ -0,0 +1,16 @@
+# create name variable of user's name using the input function
+# print the name variable
+# create age variable of user's age using the input function
+# print the age variable
+# print Hello World!
+
+# variable created containing user's name
+name = input("What is your name? ")
+print(name)
+
+# variable created containing user's age
+age = input ("What is your age? ")
+print(age)
+
+# print Hello World!
+print("Hello World!")
