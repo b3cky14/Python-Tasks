77a592eeb3fed21d4436034c1bdc276ed90f4524
b3cky14 <153016182+b3cky14@users.noreply.github.com>
2023-12-27 11:31:17 +0000

    Add files via upload

---
+++ String-and-Numerical-Data-Types/numbers.py
@@ -0,0 +1,18 @@
+# create 3 variables asking the user for a number,
+# converting each into a interger
+num1 = int(input("Choose a whole number : "))
+num2 = int(input("Choose another whole number : "))
+num3 = int(input("Choose one more whole number : "))
+
+
+# print sum of all numbers
+print(num1 + num2 + num3) 
+
+# print first number minus second number
+print(num1 - num2)
+
+# print third number multiplied by first number
+print(num3 * num1)
+
+# print the sum of all three numbers divided by third number
+print((num1 + num2 + num3) / num3)
